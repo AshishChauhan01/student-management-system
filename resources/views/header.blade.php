b7beb1b0359f74b85fcdd15720d1a9684fb79080
Gaurav-dotcom <pokhriyalgaurav94@gmail.com>
2025-08-04 17:01:21 +0530

    new project setup

---
+++ resources/views/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-4">
+                <div class="logo">
+                    <img src="{{url('assets/images/logo.png')}}" alt="">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-success">Login</button>
+            </div>
+        </div>
+    </div>
+</header>
